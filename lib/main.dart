1d8569e2ea4574cd76fdcf1f04d1e23cbee84461
dxgsltn <gulshaiym.sultan@jihc.edu.kz>
2024-06-06 19:28:17 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,26 @@
 import 'package:flutter/material.dart';
-import 'welcome_page.dart';
 import 'home_page.dart';
-import 'search_page.dart';
 import 'info_page.dart';
+import 'search.dart';
+import 'welcome_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( 
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => WelcomePage()),
-                );
-              },
-              child: Text("Go to Welcome Page"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                );
-              },
-              child: Text("Go to Home Page"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SearchPage()),
-                );
-              },
-              child: Text("Go to Search Page"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InfoPage()),
-                );
-              },
-              child: Text("Go to Info Page"),
-            ),
-          ],
-        ),
-      ),
+    return MaterialApp(
+      title: 'My App',
+      initialRoute: '/welcome', // Устанавливаем маршрут по умолчанию
+      routes: {
+        '/welcome': (context) => WelcomePage(),
+        '/home': (context) => HomePage(),
+        '/info': (context) => InfoPage(),
+        '/search': (context) => SearchPage(),
+        // Добавьте другие страницы по мере необходимости
+      },
     );
   }
 }
